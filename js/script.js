c522eef1b1929d72415f6da4b6b6989b546aa1e4
大津賢太郎 <wwwtarouwww1107@gmail.com>
2025-05-17 17:36:10 +0900

    5/17作業途中

--- js/script.js
+++ js/script.js
@@ -10,3 +10,23 @@ jQuery("#js-button-top").on("click", function () {
     500
   );
 });
+
+jQuery(".js-modal-open").on("click", function (e) {
+  e.preventDefault();
+
+  jQuery("#js-about-modal")[0].showModal();
+});
+
+jQuery(".js-modal-close").on("click", function (e) {
+  e.preventDefault();
+
+  jQuery("#js-about-modal")[0].close();
+});
+
+jQuery("#js-about-modal").on("click", function(e) {
+  // e.targetがdialog本体自身（背景）だった場合のみ閉じる
+  if (e.target === this) {
+    this.close();
+  }
+});
+
